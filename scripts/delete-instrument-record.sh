c4e7ee1a1cc8fca354d81967a284e16d14b6e467
David Roper <davidroper321@gmail.com>
2025-06-11 14:45:08 -0400

    feat: add interactive script to delete instrument records

--- scripts/delete-instrument-record.sh
+++ scripts/delete-instrument-record.sh
@@ -4,12 +4,20 @@
 
 read -p "Enter file path to docker yaml file: " docker_file
 read -p "Enter the type of MongoDB instance (development or production): " instance_type
-read -p "Enter the instrument record ID to delete: " id
+
 
 
 # Run mongosh to delete the document with the given _id
 docker compose -f $docker_file exec -T mongo mongosh --quiet --eval <<EOF
-use open-data-capture-$instance_type
-db.InstrumentRecordModel.deleteOne({ _id: ObjectId($id) })
+use data-capture-$instance_type
+db.InstrumentRecordModel.find()
+EOF
+
+
+read -p "Enter the instrument record ID to delete: " id
+
+docker compose -f $docker_file exec -T mongo mongosh --quiet --eval  <<EOF
+use data-capture-$instance_type
+db.InstrumentRecordModel.deleteOne({_id: $id})
 EOF
 
